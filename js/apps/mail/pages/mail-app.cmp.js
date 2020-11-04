407e8d95bb1bd24eeb90a5495d654d5cbb3b8af1
Matan Cohen <matanco3605@gmail.com>
2020-11-04 16:42:25 +0200

    rendered the mails!

--- js/apps/mail/pages/mail-app.cmp.js
+++ js/apps/mail/pages/mail-app.cmp.js
@@ -1,16 +1,30 @@
+import {mailService} from '../services/mail-service.js'
 import mailSideNav from '../cmps/mail-side-nav.cmp.js'
 import mailFilter from '../cmps/mail-filter.cmp.js'
+import mailList from '../cmps/mail-list.cmp.js'
 
 export default {
     template: `
         <section class="mail-page flex">
             <mail-side-nav />
             <mail-filter />
+            <mail-list :mails="mails" />
             <router-view></router-view>
         </section>
     `,
+    data() {
+        return {
+            mails: null
+        }
+    },
     components: {
         mailSideNav,
-        mailFilter
+        mailFilter,
+        mailList,
+
+    },
+    created() {
+        mailService.getMailList()
+        .then(mails => this.mails= mails)
     }
 };
