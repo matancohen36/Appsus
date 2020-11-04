d6efa918989d559ee2a20e1b3beadbfc16dad916
Inbar Tamir <inbartamir92@gmail.com>
2020-11-04 15:46:54 +0200

    navbar

--- js/apps/mail/cmps/mail-side-nav.cmp.js
+++ js/apps/mail/cmps/mail-side-nav.cmp.js
@@ -1,8 +1,14 @@
 
 export default {
     template: `
-        <section class="mail-side-nav flex">
-            <h1>Hello! I am the side nav!</h1>
+        <section class="mail-side-nav flex column align-center">
+            <button class="btn-compose-mail">+ Compose</button>
+            <router-link v-bind:to="'/mail/' + folder" v-for="folder in folders">folder</router-link>
         </section>
-    `
+    `,
+    data() {
+        return {
+            folders: [ 'Inbox', 'Starred', 'Sent', 'Drafts', 'Deleted', 'All Mail' ],
+        }
+    }
 }
--- js/apps/mail/pages/mail-app.cmp.js
+++ js/apps/mail/pages/mail-app.cmp.js
@@ -1,16 +1,16 @@
-import sideNav from '../cmps/mail-side-nav.cmp.js'
+import mailSideNav from '../cmps/mail-side-nav.cmp.js'
 import mailFilter from '../cmps/mail-filter.cmp.js'
 
 export default {
     template: `
         <section class="mail-page flex">
-            <side-nav />
+            <mail-side-nav />
             <mail-filter />
             <router-view></router-view>
         </section>
     `,
     components: {
-        sideNav,
+        mailSideNav,
         mailFilter
     }
 };
