5b2a8b01d3f3763bc288a1122eb2b0f8e590a892
Inbar Tamir <inbartamir92@gmail.com>
2020-11-04 16:10:48 +0200

    shuv commit

--- js/apps/mail/services/mail-service.js
+++ js/apps/mail/services/mail-service.js
@@ -1,13 +1,13 @@
 import utilService from '../services/utilService.js'
 const gEmails =[
-    {id:utilService.makeId(8),from:'inbar the sis',subject: 'what does the fox say?', body: 'tinitnintinitnintin!', status: [{isRead:false,starMarked:false,isDeleted:false}], sentAt : 1551133930594},
-    {id:utilService.makeId(8),from:'matan the bro',subject: 'what does the fox say?', body: 'tinitnintinitnintin!', status: [{isRead:false,starMarked:false,isDeleted:false}], sentAt : 1551133930594},
-    {id:utilService.makeId(8),from:'Napthlie the Desroyer',subject: 'what does the fox say?', body: 'tinitnintinitnintin!', status: [{isRead:false,starMarked:false,isDeleted:false}], sentAt : 1551133930594},
-    {id:utilService.makeId(8),from:'idan the famous ninja',subject: 'what does the fox say?', body: 'tinitnintinitnintin!', status: [{isRead:false,starMarked:false,isDeleted:false}], sentAt : 1551133930594},
-    {id:utilService.makeId(8),from:'Rick, diemention1',subject: 'i cant remmber which is the original bath', body: 'can u help me find out who is my actuall daughter?', status: [{isRead:false,starMarked:false,isDeleted:false}], sentAt : 1551133930594},
-    {id:utilService.makeId(8),from:'mr.meeSix',subject: 'hello im mister meeSix', body: 'hi im meesix how can i help u?', status: [{isRead:false,starMarked:false,isDeleted:false}], sentAt : 1551133930594},
-    {id:utilService.makeId(8),from:'yaron biton',subject: 'why i belive Vue is the best framework', body: 'come visit my course and find out!', status: [{isRead:false,starMarked:false,isDeleted:false}], sentAt : 1551133930594},
-    {id:utilService.makeId(8),from:'asaf marglit',subject: 'what does the fox say?', body: 'tinitnintinitnintin!', status: [{isRead:false,starMarked:false,isDeleted:false}], sentAt : 1551133930594}
+    {id:utilService.makeId(8),from:'inbar the sis',subject: 'what does the fox say?', body: 'tinitnintinitnintin!', status: {isRead:false,starMarked:false,isDeleted:false}, sentAt : 1551133930594},
+    {id:utilService.makeId(8),from:'matan the bro',subject: 'what does the fox say?', body: 'tinitnintinitnintin!', status: {isRead:false,starMarked:false,isDeleted:false}, sentAt : 1551133930594},
+    {id:utilService.makeId(8),from:'Napthlie the Desroyer',subject: 'what does the fox say?', body: 'tinitnintinitnintin!', status: {isRead:false,starMarked:false,isDeleted:false}, sentAt : 1551133930594},
+    {id:utilService.makeId(8),from:'idan the famous ninja',subject: 'what does the fox say?', body: 'tinitnintinitnintin!', status: {isRead:false,starMarked:false,isDeleted:false}, sentAt : 1551133930594},
+    {id:utilService.makeId(8),from:'Rick, diemention1',subject: 'i cant remmber which is the original bath', body: 'can u help me find out who is my actuall daughter?', status: {isRead:false,starMarked:false,isDeleted:false}, sentAt : 1551133930594},
+    {id:utilService.makeId(8),from:'mr.meeSix',subject: 'hello im mister meeSix', body: 'hi im meesix how can i help u?', status: {isRead:false,starMarked:false,isDeleted:false}, sentAt : 1551133930594},
+    {id:utilService.makeId(8),from:'yaron biton',subject: 'why i belive Vue is the best framework', body: 'come visit my course and find out!', status: {isRead:false,starMarked:false,isDeleted:false}, sentAt : 1551133930594},
+    {id:utilService.makeId(8),from:'asaf marglit',subject: 'what does the fox say?', body: 'tinitnintinitnintin!', status: {isRead:false,starMarked:false,isDeleted:false}, sentAt : 1551133930594}
 ]
 
 
