493f0c91a7d430278d80d7e8c57e7182f572fd7d
Inbar Tamir <inbartamir92@gmail.com>
2020-11-04 16:51:09 +0200

    mail icons

--- js/apps/mail/cmps/mail-side-nav.cmp.js
+++ js/apps/mail/cmps/mail-side-nav.cmp.js
@@ -3,12 +3,19 @@ export default {
     template: `
         <section class="mail-side-nav flex column align-center">
             <button class="btn-compose-mail">+ Compose</button>
-            <router-link :to="'/mail/' + folder.trim()" v-for="folder in folders" :key="folder">{{folder}}</router-link>
+            <router-link :to="'/mail/' + folder.name.toLowerCase()" v-for="folder in folders" :key="folder.name.toLowerCase()">
+                <i class="fa" :class="folder.icon"></i> {{folder.name}}</router-link>
         </section>
     `,
     data() {
         return {
-            folders: [ 'Inbox', 'Starred', 'Sent', 'Drafts', 'Deleted', 'All Mail' ],
+            folders: [
+                { name: 'Inbox', icon: 'fa-envelope-open' },
+                { name: 'Starred', icon: 'fa-star'},
+                { name: 'Sent', icon: 'fa-share' },
+                { name: 'Drafts', icon: 'fa-file' },
+                { name: 'Deleted', icon: 'fa-trash'} 
+            ],
         }
     }
 }
