5892460e26278dae2b9fb88bdfc2908fbabe8c4d
Inbar Tamir <inbartamir92@gmail.com>
2020-11-05 00:19:08 +0200

    fixed the worst bug ever

--- js/apps/mail/pages/mail-app.cmp.js
+++ js/apps/mail/pages/mail-app.cmp.js
@@ -1,7 +1,7 @@
-import {mailService} from '../services/mail-service.js'
-import mailSideNav from '../cmps/mail-side-nav.cmp.js'
-import mailFilter from '../cmps/mail-filter.cmp.js'
-import mailList from '../cmps/mail-list.cmp.js'
+import { mailService } from '../services/mail-service.js';
+import mailSideNav from '../cmps/mail-side-nav.cmp.js';
+import mailFilter from '../cmps/mail-filter.cmp.js';
+import mailList from '../cmps/mail-list.cmp.js';
 
 export default {
     name: 'mailApp',
@@ -10,7 +10,7 @@ export default {
             <mail-side-nav />
             <div class="mail-main-container flex column">
                 <mail-filter @filtered="setFilter"/>
-                <router-view :mails="mails"></router-view>
+                <router-view :mails="mailsToShow"></router-view>
             </div>
         </section>
     `,
@@ -18,10 +18,10 @@ export default {
         return {
             mails: null,
             mailId: '',
-            filterBy: { byName: '' , byStatus:'all'},
-        }
+            filterBy: { byName: '', byStatus: 'all' },
+        };
     },
-    methods:{
+    methods: {
         setFilter(filterBy) {
             this.filterBy = filterBy;
         },
@@ -32,22 +32,18 @@ export default {
             const name = this.filterBy.byName.toLowerCase();
             const isRead = (this.filterBy.byStatus === 'read') ? true : false;
             return this.mails.filter(mail => {
-                mail.from.toLowerCase().includes(name) &&
-                (
-                    this.filterBy.byStatus === 'all' ||
-                    mail.status.isRead === isRead
-                )
+                return (mail.from.toLowerCase().includes(name) ||
+                        mail.subject.toLowerCase().includes(name)) &&
+                    (
+                        this.filterBy.byStatus === 'all' ||
+                        mail.status.isRead === isRead
+                    );
             });
-            // console.log('again')
-            // return this.mails
         },
-        // mailId() {
-        //     return (this.mails) ? mailService.getMailById('jhasdf346SD') : '';
-        // }
     },
     created() {
         mailService.getMailList()
-        .then(mails => this.mails = mails)
+            .then(mails => this.mails = mails);
     },
     components: {
         mailSideNav,
