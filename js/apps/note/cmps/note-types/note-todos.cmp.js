87f097b4b3e1254ee34f8c24289de2d3c24bc37c
Inbar Tamir <inbartamir92@gmail.com>
2020-11-05 22:30:37 +0200

    editing todo

--- js/apps/note/cmps/note-types/note-todos.cmp.js
+++ js/apps/note/cmps/note-types/note-todos.cmp.js
@@ -7,17 +7,18 @@ export default {
             <h1>{{note.info.label}}</h1>
             <div v-for="todo in note.info.todos">
                 <input type="checkbox" @click="toggleDone(todo)" :checked="todo.doneAt" /> 
-                <input type="text" class="todo-item" :class="{ 'line-through': todo.doneAt }" :disabled="todo.doneAt" :value="todo.txt" />
+                <input type="text" class="todo-item" v-model="todo.txt" :class="{ 'line-through': todo.doneAt }" :disabled="todo.doneAt" />
             </div>    
         </section>
     `,
     data() {
         return {
+            
         };
     },
     methods: {
         toggleDone(todo){
-            todo.doneAt = (todo.doneAt) ? '' : Date.now();
+            todo.doneAt = (todo.doneAt) ? null : Date.now();
         }
     },
     computed: {
