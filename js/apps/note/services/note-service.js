7a1f259636b56c24d56c9e5c8e75d0d149f56de7
Matan Cohen <matanco3605@gmail.com>
2020-11-05 23:09:24 +0200

    map

--- js/apps/note/services/note-service.js
+++ js/apps/note/services/note-service.js
@@ -102,9 +102,25 @@ function getNoteList() {
 export const noteService = {
     getNoteById,
     getNoteList,
-    // toggleStarred,
+    connectGoogleApi,
     // deleteMailById,
     // getEmptyMail,
     // getFoldersMap,
     // saveMail
 };
+
+
+function connectGoogleApi() {
+    const API_KEY = 'AIzaSyC1gbyzyCipjJJGQLuBXBgAbi7zLR9PJak';
+    googleApi= `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+    return new Promise((resolve, reject) => {
+        googleApi.onload = resolve;
+        googleApi.onerror = () => reject('Google script failed to load')
+    })
+}
+
+
+
+
+
+
