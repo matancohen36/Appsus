9adcfabfa75ba00cfcef3546875093235c0685fa
Matan Cohen <matanco3605@gmail.com>
2020-11-06 04:36:44 +0200

    asd

--- js/apps/note/cmps/note-types/note-img.cmp.js
+++ js/apps/note/cmps/note-types/note-img.cmp.js
@@ -8,12 +8,20 @@ export default {
         <button class="btn-remove-note" @click="emitRemoveNote(note.id)">x</button>    
             <h1>{{note.info.title}}</h1>
 
-             <img :src="note.info.url" />
+             <img :src="note.info.url" alt=""/>
         </section>
     `,
     data() {
         return {
-            
+            type: 'noteImg',
+            isPinned: true,
+            info: {
+                url: '',
+                title: 'Me Playing Mi'
+            },
+            style: {
+                backgroundColor: '#00d'
+            }
         }
 
     },
