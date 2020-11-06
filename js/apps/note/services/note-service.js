5d6c227f794cbe64e45d8060c1bb91f7269efa48
Matan Cohen <matanco3605@gmail.com>
2020-11-06 22:23:58 +0200

    notes got local storage

--- js/apps/note/services/note-service.js
+++ js/apps/note/services/note-service.js
@@ -1,153 +1,150 @@
 import { utilService } from '../../../services/util-service.js';
-const gNotes = [
-    {
-        id: utilService.makeId(),
-        type: 'note-txt',
-        status: {
-            pinned: false,
-            marked: false,
-            editMode: false,
-        },
-        info: {
-            txt: 'Fullstack Me Baby!',
-        },
-        styles: {
-            backgroundColor: '',
-        },
-    }
-
-    ,
-    {
-        id: utilService.makeId(),
-        type: 'note-img',
-        status: {
-            pinned: false,
-            marked: false,
-            editMode: false,
-        },
-
-        styles: {
-            backgroundColor: '#00d'
-        },
-        info: {
-            src: 'js/apps/note/assets/img/meme.jpg',
-            title: 'when we made the memegen'
-        },
-    },
-    {
-        id: utilService.makeId(),
-        type: 'note-todos',
-        status: {
-            pinned: false,
-            marked: false,
-            editMode: false,
-        },
-        styles: {
-            backgroundColor: '#00d',
-        },
-        info: {
-            title: 'How was it:',
-            todos: [
-                { id: utilService.makeId(), txt: 'Do that', doneAt: null },
-                { id: utilService.makeId(), txt: 'Do this', doneAt: null }
-            ]
+const STORAGE_KEY = 'noteDB';
+var gNotes;
+function getDefaultNotes(){
+    return [
+        {
+            id: utilService.makeId(),
+            type: 'note-txt',
+            status: {
+                pinned: false,
+                marked: false,
+                editMode: false,
+            },
+            info: {
+                txt: 'Fullstack Me Baby!',
+            },
+            styles: {
+                backgroundColor: '',
+            },
         }
-    },
-
-    {
-        id: utilService.makeId(),
-        type: 'note-video',
-        status: {
-            pinned: false,
-            marked: false,
-            editMode: false,
+    
+        ,
+        {
+            id: utilService.makeId(),
+            type: 'note-img',
+            status: {
+                pinned: false,
+                marked: false,
+                editMode: false,
+            },
+    
+            styles: {
+                backgroundColor: '#00d'
+            },
+            info: {
+                src: 'js/apps/note/assets/img/meme.jpg',
+                title: 'when we made the memegen'
+            },
         },
-        styles: {
-            backgroundColor: '#00d',
+        {
+            id: utilService.makeId(),
+            type: 'note-todos',
+            status: {
+                pinned: false,
+                marked: false,
+                editMode: false,
+            },
+            styles: {
+                backgroundColor: '#00d',
+            },
+            info: {
+                title: 'How was it:',
+                todos: [
+                    { id: utilService.makeId(), txt: 'Do that', doneAt: null },
+                    { id: utilService.makeId(), txt: 'Do this', doneAt: null }
+                ]
+            }
         },
-        info: {
-            url: 'https://www.youtube.com/embed/qeF3Sx_IGvE',
-            title: 'שלום בן טוב ושמן שלי '
+    
+        {
+            id: utilService.makeId(),
+            type: 'note-video',
+            status: {
+                pinned: false,
+                marked: false,
+                editMode: false,
+            },
+            styles: {
+                backgroundColor: '#00d',
+            },
+            info: {
+                url: 'https://www.youtube.com/embed/qeF3Sx_IGvE',
+                title: 'שלום בן טוב ושמן שלי '
+            }
+        },
+        {
+            id: utilService.makeId(),
+            type: 'note-video',
+            status: {
+                pinned: false,
+                marked: false,
+                editMode: false,
+            },
+            styles: {
+                backgroundColor: '#00d',
+            },
+            info: {
+                url: 'https://giphy.com/embed/1wqqlaQ7IX3TXibXZE',
+                title: ' אני אחרי שירון אמר שהכל במצגת'
+            }
+        },
+        {
+            id: utilService.makeId(),
+            type: 'note-audio',
+            status: {
+                pinned: false,
+                marked: false,
+                editMode: false,
+            },
+            styles: {
+                backgroundColor: '#00d',
+            },
+            info: {
+                src: 'js/apps/note/assets/mp3/song.mp3',
+                title: 'We will Rock You'
+            }
         }
-    },
-    {
-        id: utilService.makeId(),
-        type: 'note-video',
-        status: {
-            pinned: false,
-            marked: false,
-            editMode: false,
-        },
-        styles: {
-            backgroundColor: '#00d',
-        },
-        info: {
-            url: 'https://giphy.com/embed/1wqqlaQ7IX3TXibXZE',
-            title: ' אני אחרי שירון אמר שהכל במצגת'
-        }
-    },
-    {
-        id: utilService.makeId(),
-        type: 'note-audio',
-        status: {
-            pinned: false,
-            marked: false,
-            editMode: false,
-        },
-        styles: {
-            backgroundColor: '#00d',
-        },
-        info: {
-            src: 'js/apps/note/assets/mp3/song.mp3',
-            title: 'We will Rock You'
-        }
-    }
+    
+    
+    ]
+}
 
-
-
-];
 
 
 function getNoteById(noteId) {
     const wantedNote = gNotes.find(note => note.id === noteId);
     const note = JSON.parse(JSON.stringify(wantedNote));
+
     return Promise.resolve(note);
 }
 function getNoteList() {
-    const noteList = JSON.parse(JSON.stringify(gNotes));
-    return Promise.resolve(noteList);
+    gNotes = utilService.loadFromStorage(STORAGE_KEY)
+    if (!gNotes || !gNotes.length) {
+        gNotes = getDefaultNotes();
+        _saveNotesToStorage();
+    }
+    return Promise.resolve(gNotes)
+
 }
+
+    
+
 
 function saveNote(note) {
     const noteIdx = gNotes.findIndex(currNote => currNote.id === note.id);
     if (noteIdx >= 0) gNotes.splice(noteIdx, 1, note)
     else gNotes.unshift(note);
-    console.log('gNotes:', gNotes)
-}
-
-function addTodo(noteId) {
-    const currNote = gNotes.find(note => note.id === noteId);
-    debugger
-    currNote.info.todos.push(_getEmptyTodo());
+    _saveNotesToStorage();
 }
 
 function removeNote(noteId) {
     const idx = gNotes.findIndex(note => note.id === noteId);
     if (idx >= 0) gNotes.splice(idx, 1);
+    _saveNotesToStorage();
+
 }
 
-function removeTodo(ids) {
-    debugger;
-    const currNote = gNotes.find(note => note.id === ids.noteId);
-    const todoIdx = currNote.info.todos.findIndex(todo => todo.id === ids.todoId);
-    console.log('todoIdx:', todoIdx);
-    if (todoIdx >= 0) currNote.info.todos.splice(todoIdx, 1);
-    saveNote(currNote);
-}
-
-function _getEmptyTodo() {
-    return { id: utilService.makeId(), txt: '', doneAt: null };
-}
 
 function getEmptyNote() {
     return {
@@ -193,6 +190,32 @@ function buildNote(newNote, noteInfo) {
 
 
 
+
+
+// NOTE - TODO!
+function addTodo(noteId) {
+    const currNote = gNotes.find(note => note.id === noteId);
+    debugger
+    currNote.info.todos.push(_getEmptyTodo());
+}
+
+function _getEmptyTodo() {
+    return { id: utilService.makeId(), txt: '', doneAt: null };
+}
+function removeTodo(ids) {
+    debugger;
+    const currNote = gNotes.find(note => note.id === ids.noteId);
+    const todoIdx = currNote.info.todos.findIndex(todo => todo.id === ids.todoId);
+    console.log('todoIdx:', todoIdx);
+    if (todoIdx >= 0) currNote.info.todos.splice(todoIdx, 1);
+    saveNote(currNote);
+}
+
+
+
+
+
+
 export const noteService = {
     getNoteById,
     getNoteList,
@@ -225,3 +248,6 @@ export const noteService = {
 
 
 
+function _saveNotesToStorage() {
+    utilService.storeToStorage(STORAGE_KEY, gNotes)
+}
