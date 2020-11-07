3fb41a31d6b73d9aed85f89668da13673c39a680
Inbar Tamir <inbartamir92@gmail.com>
2020-11-07 02:42:23 +0200

    notes please be nice

--- js/apps/note/cmps/note-list.cmp.js
+++ js/apps/note/cmps/note-list.cmp.js
@@ -4,7 +4,7 @@ export default {
     name: 'noteList',
     props: ['notes'],
     template: `
-            <section class="notes-list flex wrap">
+            <section class="notes-list">
                 <note-preview v-for="currNote in notes" :key="currNote.id" :note="currNote" />
             </section>
     `,
