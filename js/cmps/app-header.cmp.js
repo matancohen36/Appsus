1bd3640ba75d49906fa2f72a689d592fc5af5104
Matan Cohen <matanco3605@gmail.com>
2020-11-12 16:58:17 +0200

    hamburger

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,25 +1,39 @@
 export default {
     name: 'appHeader',
     template: `
-                <header class="app-header">
-                    <nav class="clean-list flex space-evenly align-center">
-                        <li>
+                <header class="app-header flex row-reverse ">
+                <div v-show="showMenu" class="close-menu" @click="toggleMenu()"></div>
+                    <nav v-show="showMenu" class="clean-list flex column space-evenly align-center">
+                        <li @click="toggleMenu()">
                             <router-link to="/" exact>Home</router-link>
                         </li>
-                        <li>
+                        <li @click="toggleMenu()">
                             <router-link to="/book" exact>Book </router-link>
-                        </li>
-                        <li>
+                        </li >
+                        <li @click="toggleMenu()">
                             <router-link to="/note" exact>Note </router-link>
                         </li>
-                        <li>
+                        <li @click="toggleMenu()">
                             <router-link to="/mail" exact>Email </router-link>
                         </li>
-                        <li>
+                        <li @click="toggleMenu()">
                             <router-link to="/about" exact>About </router-link>
                         </li>
                     </nav>
+                    <button v-show="!showMenu" class="menu-btn" @click="toggleMenu()">☰</button>
                 </header>
+            
             `
+    , data() {
+        return {
+            showMenu: false
+        }
+    }
+    , methods: {
+        toggleMenu(){
+            this.showMenu =!this.showMenu
+        }
+    },
+
 }
 
